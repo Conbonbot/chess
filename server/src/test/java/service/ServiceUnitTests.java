f2c92942e0ccfe95d2259c0f9b5cc5f82988fb4e
Connor Harvey <conbonbot745@gmail.com>
2025-02-13 18:30:40 -0700

    Added inital service unit tests.

--- server/src/test/java/service/ServiceUnitTests.java
+++ server/src/test/java/service/ServiceUnitTests.java
@@ -1,41 +1,110 @@
 package service;
 
 
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
- import org.junit.jupiter.api.Order;
- import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-import server.Server;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import requests.Request;
+
 
 /**
  *
  * @author Connor
  */
+
+ @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ServiceUnitTests{
 
-    private static Server server;
+    private static ChessService service;
 
-    @AfterAll
-    static void stopServer(){
-        server.stop();
-    }
+    private String authToken;
 
     @BeforeAll
     public static void init(){
-        server = new Server();
-        var port = server.run(3000);
-        System.out.println("Started test HTTP server on " + port);
-        // Add init for users
+        service = new ChessService(new MemoryAuthDAO(), new MemoryGameDAO(), new MemoryUserDAO());
     }
+
+    @BeforeEach
+    public void setup(){
+        service.clear(new Request.Delete());
+
+        // Log in user
+        Request.Register regReq = new Request.Register("username", "password", "example@email.com");
+        var regRes = service.register(regReq);
+
+        authToken = regRes.authToken();
+    }
+    
+    // positive register
+    @Test
+    @DisplayName("Normal user registration")
+    public void positiveRegister(){
+        Request.Register regReq = new Request.Register("username1", "password1", "example1@email.com");
+
+        var regRes = service.register(regReq);
+
+        Assertions.assertTrue(regRes.errorMessage().isEmpty());
+    }
+    
+    // negative register
+    @Test
+    @DisplayName("Invalid user registration")
+    public void invalidRegister(){
+        Request.Register regReq1 = new Request.Register("username1", "password1", "example1@email.com");
+        Request.Register regReq2 = new Request.Register("username1", "password1", "example2@email.com");
+
+        service.register(regReq1);
+        var regRes2 = service.register(regReq2);
+
+        Assertions.assertFalse(regRes2.errorMessage().isEmpty());
+    }
+    
+    // positive login
+    @Test
+    @DisplayName("Normal user login")
+    public void normalLogin(){
+        Request.Login logReq = new Request.Login("username", "password");
+
+        var logRes = service.login(logReq);
+
+        Assertions.assertTrue(logRes.username().equals("username"));
+    }
+
+    // negative login
+    @Test
+    @DisplayName("Invalid user login")
+    public void invalidLogin(){
+        Request.Login logReq = new Request.Login("username", "password?");
+
+        var logRes = service.login(logReq);
+
+        Assertions.assertFalse(logRes.username() != null && logRes.authToken() != null);
+    }
+
+    // positive logout
+    // negative logout
+
+    // Positive showGames
+    // Negative showGames
+
+    // positive createGame
+    // negative createGame
+
+    // positive joinGame
+    // negative joinGame
+
+    // Positive clear
+    // Negative clear
 
     
-    @Test
-    @Order(1)
-    @DisplayName("Clear Service")
-    public void clearService() {
-        
-    }
 
+    
 }
