f047dfc473db2da4df6faec1d94d87ea73cb943e
Connor Harvey <conbonbot745@gmail.com>
2025-02-21 10:37:28 -0700

    Updated userDAO for SQL table creation

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,5 +1,7 @@
 package dataaccess;
 
+import java.sql.SQLException;
+
 import model.UserData;
 
 
@@ -27,12 +29,35 @@ public class MySqlUserDAO implements UserDAO{
         throw new UnsupportedOperationException("Unimplemented method 'clear'");
     }
 
+    private final String[] createTable = {
+        """
+        CREATE TABLE IF NOT EXISTS user (
+          `username` varchar(256) NOT NULL,
+          `password` varchar(256) NOT NULL,
+          `email` varchar(256) NOT NULL,
+          PRIMARY KEY (`username`)
+        );
+        """
+    };
+
     private void configureDatabase(){
         try{
             DatabaseManager.createDatabase();
         }
-        catch(DataAccessException e){
-            
+        catch(DataAccessException ex){}
+        try {
+            var conn = DatabaseManager.getConnection();
+            for(var statment : createTable){
+                
+                try(var sanatized = conn.prepareStatement(statment)){
+                    sanatized.executeUpdate();
+                }
+                catch(SQLException ex){
+                    System.out.println(ex.toString());
+                }
+            }
+        } 
+        catch (DataAccessException e) {
         }
     }
 
