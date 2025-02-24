aa658e5ededb39dd2e2bcd2d07ad3df5e364a352
Connor Harvey <conbonbot745@gmail.com>
2025-02-23 22:30:23 -0700

    Updated for basic error handling.

--- client/src/main/java/Console.java
+++ client/src/main/java/Console.java
@@ -16,6 +16,8 @@ public class Console {
     private static String status = EscapeSequences.SET_TEXT_COLOR_RED + "[LOGGED_OUT]" + EscapeSequences.FULL_COLOR_RESET;
     private static boolean console = true;
     private static boolean loggedIn = false;
+    // Change to be an instance
+    private static String authToken = "";
 
     private static void init(){
         System.out.printf("%s%s Welcome to 240 Chess. Type Help to get started %s%n", 
@@ -28,29 +30,37 @@ public class Console {
         scanner = new Scanner(System.in);
     }
 
-    public static void run() throws Exception{
+    public static void run(){
         init();
         while (!line.toLowerCase().equals("quit")) {
             System.out.printf("%s >>> ", status);
             if(console && scanner.hasNextLine()) {
                 line = scanner.nextLine();
-                switch(line.split(" ")[0]) {
-                    // Commands
-                    case "quit" -> quit();
-                    case "help" -> help();
-                    // Prelogin commands
-                    case "login" -> login();
-                    case "register" -> register();
-                    // Postlogin commands
-                    case "logout" -> logout();
-                    case "create" -> createGame();
-                    case "list" -> listGames();
-                    case "observe" -> observeGame();
-                    case "join" -> joinGame();
-                    default -> System.out.printf("%s'%s' is not recognized as a command. Type help for a list%s%n",
-                                EscapeSequences.SET_TEXT_COLOR_RED, 
-                                line,
-                                EscapeSequences.RESET_TEXT_COLOR);
+                try{
+                    switch(line.split(" ")[0]) {
+                        // Commands
+                        case "quit" -> quit();
+                        case "help" -> help();
+                        // Prelogin commands
+                        case "login" -> login();
+                        case "register" -> register();
+                        // Postlogin commands
+                        case "logout" -> logout();
+                        case "create" -> createGame();
+                        case "list" -> listGames();
+                        case "observe" -> observeGame();
+                        case "join" -> joinGame();
+                        default -> System.out.printf("%s'%s' is not recognized as a command. Type help for a list%s%n",
+                                    EscapeSequences.SET_TEXT_COLOR_RED, 
+                                    line,
+                                    EscapeSequences.RESET_TEXT_COLOR);
+                    }
+                }
+                catch(IOException ex){
+                    exceptionHandler(ex);
+                }
+                catch(Exception ex){
+                    exceptionHandler(ex);
                 }
             }
             else{
@@ -60,6 +70,21 @@ public class Console {
             }
         }
     }
+
+    private static void exceptionHandler(IOException ex){
+        switch (Integer.parseInt(ex.getMessage())) {
+            case 400 -> System.out.printf("%sError: bad request%s%n", EscapeSequences.SET_TEXT_COLOR_RED, EscapeSequences.FULL_COLOR_RESET);
+            case 401 -> System.out.printf("%sYou are unauthorized.%s%n", EscapeSequences.SET_TEXT_COLOR_RED, EscapeSequences.FULL_COLOR_RESET);
+            case 403 -> System.out.printf("%sThis has already been taken.%s%n", EscapeSequences.SET_TEXT_COLOR_RED, EscapeSequences.FULL_COLOR_RESET);
+            default -> System.out.printf("%sAn error has occured. Try again.%s%n", EscapeSequences.SET_TEXT_COLOR_RED, EscapeSequences.FULL_COLOR_RESET);
+        }
+    }
+
+    private static void exceptionHandler(Exception ex){
+
+    }
+
+
 
     // Prelogin
 
@@ -110,35 +135,48 @@ public class Console {
         
     }
 
-    private static void login(){
-        System.out.println("So you want to login?");
+    private static void login() throws Exception{
+        if(line.split(" ").length == 3){
+            var values = line.split(" ");
+            var body = Map.of("username", values[1], "password", values[2]);
+            HttpURLConnection http = sendRequest("http://localhost:3000/session", "POST", new Gson().toJson(body));
+            String response = receiveResponse(http).toString();
+            authToken = response.substring(response.indexOf("authToken")+10, response.length()-1);
+            loggedIn = true;
+            status = EscapeSequences.SET_TEXT_COLOR_GREEN + "[LOGGED_IN]" + EscapeSequences.FULL_COLOR_RESET;
+        }
+        else{
+            System.out.printf("Something is missing from your request.%nThe format is <USERNAME> <PASSWORD>%n");
+        }
     }
 
     private static void register() throws Exception{
         if(line.split(" ").length == 4){
-            System.out.printf("Fantastic%n");
-            loggedIn = true;
-            status = EscapeSequences.SET_TEXT_COLOR_GREEN + "[LOGGED_IN]" + EscapeSequences.FULL_COLOR_RESET;
             // make HTTP protocol
             var values = line.split(" ");
             var body = Map.of("username", values[1], "password", values[2], "email", values[3]);
             HttpURLConnection http = sendRequest("http://localhost:3000/user", "POST", new Gson().toJson(body));
             receiveResponse(http);
+
+            loggedIn = true;
+            status = EscapeSequences.SET_TEXT_COLOR_GREEN + "[LOGGED_IN]" + EscapeSequences.FULL_COLOR_RESET;
         }
         else{
-            System.out.printf("something is missing%n");
+            System.out.printf("Something is missing%nThe format is <USERNAME> <PASSWORD> <EMAIL>%n");
         }
     }
 
     // Postlogin
 
-    private static void logout(){
+    private static void logout() throws Exception{
         if(loggedIn){
+            // make request
+
             loggedIn = false;
             status = "[LOGGED_OUT]";
         }
         else{
-            System.out.printf("Hold on partner%n");
+            System.out.printf("You have to log in first%n");
         }
     }
 
@@ -198,16 +236,21 @@ public class Console {
         }
     }
 
-    private static void receiveResponse(HttpURLConnection http) throws IOException {
-        var statusCode = http.getResponseCode();
-        var statusMessage = http.getResponseMessage();
+    private static Object receiveResponse(HttpURLConnection http) throws IOException {
+        int statusCode = http.getResponseCode();
+        String statusMessage = http.getResponseMessage();
+
+        if(statusCode != 200){
+            throw new IOException(Integer.toString(statusCode));
+        }        
 
         Object responseBody = readResponseBody(http);
         System.out.printf("= Response =========\n[%d] %s\n\n%s\n\n", statusCode, statusMessage, responseBody);
+        return responseBody;
     }
 
     private static Object readResponseBody(HttpURLConnection http) throws IOException {
-        Object responseBody = "";
+        Object responseBody;
         try (InputStream respBody = http.getInputStream()) {
             InputStreamReader inputStreamReader = new InputStreamReader(respBody);
             responseBody = new Gson().fromJson(inputStreamReader, Map.class);
